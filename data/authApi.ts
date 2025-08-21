7c4d1c07d7e0f0684f47e02e196e916a569c4243
RoaTheDev <roachamreun15@proton.me>
2025-08-21 09:04:21 +0700

    remove hard coded api path

--- data/authApi.ts
+++ data/authApi.ts
@@ -8,30 +8,28 @@ import {
 } from "@/types/authTypes";
 import {ApiResponse} from "@/types/commonTypes";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
-
 export const loginApi = async (email: string, password: string): Promise<ApiResponse<LoginResponse>> => {
-    const response = await axios.post(`${API_BASE_URL}/auth/login`, {email, password});
+    const response = await axios.post(`/api/auth/login`, {email, password});
     return response.data;
 };
 
 export const signupApi = async (data: ServerSignupFromData): Promise<ApiResponse<SignupResponse>> => {
-    const response = await axios.post(`${API_BASE_URL}/auth/signup`, data);
+    const response = await axios.post(`/api/auth/signup`, data);
     return response.data;
 };
 
 export const logoutApi = async () => {
-    const response = await axios.post(`${API_BASE_URL}/auth/logout`, {withCredentials: true});
+    const response = await axios.post(`/api/auth/logout`, {withCredentials: true});
     return response.data;
 };
 
 export const forgetPasswordApi = async (email: string): Promise<ApiResponse<ForgetPasswordResponse>> => {
-    const response = await axios.post(`${API_BASE_URL}/auth/forgot-password`, {email});
+    const response = await axios.post(`/api/auth/forgot-password`, {email});
     return response.data;
 };
 
 export const resetPasswordApi = async (sessionId: string, otp: string, newPassword: string) => {
-    const response = await axios.post(`${API_BASE_URL}/auth/reset-password`, {sessionId, otp, newPassword});
+    const response = await axios.post(`/api/auth/reset-password`, {sessionId, otp, newPassword});
     return response.data;
 };
 
@@ -41,16 +39,16 @@ export const updateProfileApi = async (data: {
     email: string;
     phone?: string | null;
 }): Promise<ApiResponse<UserType>> => {
-    const response = await axios.put(`${API_BASE_URL}/user`, data, {withCredentials: true});
+    const response = await axios.put(`/api/user`, data, {withCredentials: true});
     return response.data;
 };
 
 export const fetchUserProfile = async (): Promise<UserType> => {
-    const res = await axios.get(`${API_BASE_URL}/user`, {withCredentials: true});
-    return res.data
+    const res = await axios.get(`/api/user`, {withCredentials: true});
+    return res.data;
 };
 
 export const ValidateSession = async (): Promise<string> => {
-    const res = await axios.get(`${API_BASE_URL}/validate`, {withCredentials: true});
-    return res.data
+    const res = await axios.get(`/api/validate`, {withCredentials: true});
+    return res.data;
 };
